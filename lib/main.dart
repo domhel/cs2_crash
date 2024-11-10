4984d43a18c939ebb81e7e6064d4096028392d74
Dominik Helfenstein <dominik.helfenstein@gmx.de>
2024-11-10 14:37:27 +0100

    naming stuff

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         brightness: Brightness.dark,
         colorSchemeSeed: Colors.black,
